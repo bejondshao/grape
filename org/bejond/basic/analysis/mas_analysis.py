a194b66fb5f1134a92f889ed8eaf60116d2fa48b
Bejond Shao <bejond@163.com>
2017-10-13 00:22:47 +0800

    master find_head_up()

--- org/bejond/basic/analysis/mas_analysis.py
+++ org/bejond/basic/analysis/mas_analysis.py
@@ -6,3 +6,9 @@ from bejond.basic import conn, const
 
 def find_head_up():
     codes = conn.collection_stock_basics.find({}, {'code': 1, '_id': 0})
+    for code in codes:
+        histories = list(conn.collection_stock_hist.find({'code': code}).sort([('date', 1)]).limit(60))
+
+        for history in histories:
+            date = history['date']
+            ma_60 = history['ma_60']
